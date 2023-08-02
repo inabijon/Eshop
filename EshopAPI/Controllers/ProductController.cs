d44d76324412cb8df02eae71cd534ae53b1e0d68
NABIJON <nabijon.ibodullaev@esynergy.lv>
2023-08-02 09:56:29 +0500

    Add project files.

---
+++ EshopAPI/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EshopAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController: ControllerBase 
+    {
+
+        [HttpGet]
+        public IActionResult Get() { 
+           return Ok("Hello World");
+        }
+    }
+    
+}
